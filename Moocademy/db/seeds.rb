1fe3a1194f1655ad1a295cf24f690c6dcc308ed3
Jooksta <teslachipset@gmail.com>
2018-10-24 23:11:33 +0200

    modif faker

--- Moocademy/db/seeds.rb
+++ Moocademy/db/seeds.rb
@@ -9,9 +9,9 @@
 require 'faker'
 
 10.times do
-    course = Course.create!(title: Faker::GitClass.title, content: Faker::GitClass.quote)
+    course = Course.create!(title: Faker::Games::Zelda.game, content: Faker::Games::Zelda.item)
   end
 
 10.times do
-    lesson = Lesson.create!(course_id: Faker::Number.between(1,10), title: Faker::GitPush.title, body: Faker::HowToDo.quote)
+    lesson = Lesson.create!(course_id: Faker::Number.between(1,10), title: Faker::Games::Zelda.game, body: Faker::Games::Zelda.item)
 end
