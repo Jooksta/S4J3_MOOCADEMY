96e582a0de65f3ec68756601ae6c32d3369dc5dc
Jooksta <teslachipset@gmail.com>
2018-10-24 19:42:44 +0200

    exo fini

--- Moocademy/db/seeds.rb
+++ Moocademy/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+    course = Course.create!(title: Faker::GitClass.title, content: Faker::GitClass.quote)
+  end
+
+10.times do
+    lesson = Lesson.create!(course_id: Faker::Number.between(1,10), title: Faker::GitPush.title, body: Faker::HowToDo.quote)
+end
